712e34fba38393092bf0268f6ddde6bf6718fa47
MalluCortez <maria.santana@outlook.com.br>
2022-09-30 15:34:48 -0300

    aletrações requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,15 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-
-  /* if (ids !== data.species.id){
-    return []}
-     data.species.forEach((specie) => {} ) */
-
-    const arrAnimals = ids.map((i) => data.species.find((species) => species.id === i));
-    const animalsId = (ids.length < 1) ? [] : arrAnimals;
-    return animalsId
-  };
-
-console.log(data.species[0].id)
+  const arrAnimals = ids.map((i) => data.species.find((specie) => specie.id === i));
+  const animalsId = (ids.length === 0) ? [] : arrAnimals;
+  return animalsId;
+}
 module.exports = getSpeciesByIds;
