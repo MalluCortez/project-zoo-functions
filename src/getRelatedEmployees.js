03ec005a391ed13af79747d479747e3a87fe66e9
MalluCortez <maria.santana@outlook.com.br>
2022-10-04 20:38:26 -0300

    requisito 4 feito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((element) => element.managers.some((e) => e === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const findSubordinates = data.employees.filter((e) =>
+    e.managers.some((s) => s === managerId))
+    .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return findSubordinates;
 }
 
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
