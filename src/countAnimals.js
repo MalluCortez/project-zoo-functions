b8c355e45cc14c423362caa7898257b5c25923ad
MalluCortez <maria.santana@outlook.com.br>
2022-10-04 23:39:41 -0300

    requisito 5 feito parcialmente

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  /* const quantity = data.species.find((element) => element.residents).residents.length; */
+  /*   const nameAnimals = data.species.find((element) => element.name).name;
+    const objetoQuantAnimals = {
+      animal: data.species.find((element) => element.residents).residents.length,
+    };
+    return objetoQuantAnimals; */
+
+  const object = {};
+  data.species.forEach((a) => {
+    object[a.name] = a.residents.length;
+  });
+
+  if (!animal) return object;
 }
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
