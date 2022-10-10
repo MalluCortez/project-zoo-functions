9b9e411d55fbb0465733549d62d197d2fe91acb6
MalluCortez <maria.santana@outlook.com.br>
2022-10-10 13:34:44 -0300

    requisito 10 feito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const findFirstSpecie = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const oldestAnimal = data.species
+    .find((specie) => specie.id === findFirstSpecie).residents.reduce((acc, specie) => {
+      const specieAge = acc.age > specie.age ? acc : specie;
+      return specieAge;
+    });
+  return Object.values(oldestAnimal);
 }
 
+console.log(getOldestFromFirstSpecies('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 module.exports = getOldestFromFirstSpecies;
