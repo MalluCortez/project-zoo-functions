bc6c011884a47233f772c0b82b5b9a3ca794c7c1
MalluCortez <maria.santana@outlook.com.br>
2022-10-10 13:40:18 -0300

    retirando um código

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,7 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  return {
-    child: entrants.filter((entrant) => entrant.age < 18),
-    adult: entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50),
-    senior: entrants.filter((entrant) => entrant.age >= 50),
-  };
+
 }
 
 function calculateEntry(entrants) {
